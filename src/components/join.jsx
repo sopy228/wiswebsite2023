3f3a17e718ba693df51e0294b8910f434c29fa1c
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2023-09-25 21:59:52 -0400

    join:embedded form, removed button, link in header

--- src/components/join.jsx
+++ src/components/join.jsx
@@ -4,25 +4,29 @@ export const Join = (props) => {
   return (
     <div id='join' className='text-center'>
       <div className='section-title'>
-        <h2>Let's Stay Connected</h2>
+        <h2><a href="https://docs.google.com/forms/d/e/1FAIpQLSeCnMi2i9UfambAA0zzYpcLnzS0KnSpizlc9cMJliXwWEes_Q/viewform" target="_blank">Let's Stay Connected</a></h2>
         <p>
-          Ready to take a stand against the gender disparities in STEM?
+          Ready to take a stand against the gender disparities in STEM? Join our club.
         </p>
-        &nbsp;
+        {/* &nbsp; */}
         <div className='row'>
           <>
-
-            <Button className='btn btn-custom btn-lg' href = 'https://docs.google.com/forms/d/1ghO2tVJLUGkmqyJnHG9dHQAdMSg6LdmlHSmpyYeSvrM/viewform?edit_requested=true&fbclid=IwAR3H7Ucc2sMvQusR0Ln45Bx8oKUF9u1p2dVSPVtitjouGodRCWyiAm8Z1EE'>
-              Join our Team.
-            </Button>{''}
+            {/* <Button className='btn btn-custom btn-lg' href = 'https://docs.google.com/forms/d/e/1FAIpQLSeCnMi2i9UfambAA0zzYpcLnzS0KnSpizlc9cMJliXwWEes_Q/viewform'>
+              Join our Club.
+            </Button>{''} */}
           </>
         </div>
-        <div>
-          &nbsp;
-        <p>
-          {/* Don't miss any important info, sign up for our montly newsletter! */}
-        </p>
+        &nbsp;
+        <div className='row'>
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeCnMi2i9UfambAA0zzYpcLnzS0KnSpizlc9cMJliXwWEes_Q/viewform?embedded=true" width="700" height="400" frameborder="0" marginheight="0" marginwidth="0" title="member-signup">
+            Loading…
+          </iframe>
         </div>
+        {/*<div>
+          <p>
+            Don't miss any important info, sign up for our montly newsletter!
+          </p>
+          </div> */}
       </div>
     </div>
   )
