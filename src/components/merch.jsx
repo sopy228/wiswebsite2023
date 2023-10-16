103c3a7aef3071d57a47f249f938ba741f3d9525
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2023-10-16 14:33:10 -0400

    finishing merch section, may change images later

--- src/components/merch.jsx
+++ src/components/merch.jsx
@@ -8,15 +8,18 @@ export const Merch = (props) => {
         <div className='section-title'>
           <h2>Merchandise</h2>
           <p>
-           Come and purchase some of our exclusive WiS merchandise!
+           Come purchase some of our exclusive WiS merchandise!<br />
+           Select a piece of merch to be redirected to our purchase form.
           </p>
         </div>
         <div className='row'>
           <div className='merch-item'>
             {props.data
               ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
-                  <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+                <div key={`${d.title}-${i}`} className='col-sm-5 col-md-4 col-lg-3'>
+                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSdaLegZ3wQALxAeX7pEadiVRX5OzURmX0hh_7Oj0-UmodaSbw/viewform" target="_blank">
+                    <Image title={d.title} smallImage={d.smallImage} />
+                  </a>
                 </div>
               ))
               : 'Loading...'}
